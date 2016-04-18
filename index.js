0bf4a4db3760e2a798d711b81f6f2af9b2635c32
MARIA ROMERO <mariaromero18e@gmail.com>
2016-04-17 21:41:30 -0700

    export get request functionality

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+// module.exports = exports = require(__dirname + '/lib/router');
+// module.exports = exports = require(__dirname + '/lib/server');
+const server = require(__dirname + '/lib/server');
+exports.router = require(__dirname + '/lib/server');
 
-// const server = require(__dirname + '/lib/server');
 
-server(3000);
-
-module.exports = exports = require(__dirname + '/lib/server');
-module.exports = exports = require(__dirname + '/lib/router'); 8af9eeeba36ffa0668b3e48fcaea93e18038cbe1
+server.launch(3000);
+server.getRequest('/soeffervescent', 'default', 'text/plain', 'anybody got a scoby?');
