7eaa22d4d29663cd44f14a329a0133f43b33b655
MARIA ROMERO <mariaromero18e@gmail.com>
2016-04-18 12:47:43 -0700

    remove files

--- index.js
+++
@@ -1,7 +0,0 @@
-
-// const server = require(__dirname + '/lib/server');
-
-server(3000);
-
-module.exports = exports = require(__dirname + '/lib/server');
-module.exports = exports = require(__dirname + '/lib/router'); 8af9eeeba36ffa0668b3e48fcaea93e18038cbe1
