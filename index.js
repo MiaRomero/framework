fc0569b0b154d9d816386561dcfbe6a409acdd47
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-04-18 10:29:00 -0700

    adding files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 // const server = require(__dirname + '/lib/server');
 
-server(3000);
+// server(3000);
 
 module.exports = exports = require(__dirname + '/lib/server');
-module.exports = exports = require(__dirname + '/lib/router'); 8af9eeeba36ffa0668b3e48fcaea93e18038cbe1
+// module.exports = exports = require(__dirname + '/lib/router');
