dd20f61ae031bca27a095b4d0f4e2b35fd13cb24
David Michael Zalk <dzknj@outlook.com>
2016-04-19 14:57:22 -0700

    tweaked functions.js a bit, also changed the explanations in functions.txt to reflect these changes, and also added a server(3000) on index.js so i could get the functions to work

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-
+var server = require(__dirname + '/lib/server.js');
+var functions = require(__dirname + '/lib/functions.js');
+var Router = require(__dirname + '/lib/router.js');
 // const server = require(__dirname + '/lib/server');
-
-// server(3000);
+server.listen(3000)
 
 module.exports = exports = require(__dirname + '/lib/server');
 // module.exports = exports = require(__dirname + '/lib/router');
