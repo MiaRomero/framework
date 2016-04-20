93f31ebfd63a9fe6a62244fe208c18a1a307ce5b
MARIA ROMERO <mariaromero18e@gmail.com>
2016-04-19 19:15:29 -0700

    correct GET and 404 tests

--- test/server_test.js
+++ test/server_test.js
@@ -5,7 +5,7 @@ const expect = chai.expect;
 const request = chai.request;
 const kombucha = require(__dirname + '/../lib/server');
 
-describe('the server', () => {
+describe('the server GET requests', () => {
   before( () => {
     kombucha.listen(3000, () => {
       process.stdout.write('server up');
@@ -21,10 +21,22 @@ describe('the server', () => {
       done();
     });
   });
+  after( () => {
+    kombucha.server.close();
+  });
+});
+
+describe('the server POST requests', () => {
+  before( () => {
+    kombucha.listen(3000, () => {
+      process.stdout.write('server up');
+    });
+  });
+
   it('should accept POST requests to /soeffervescent', (done) => {
-    request('localhost:3000')
+  request('localhost:3000')
     .post('/soeffervescent')
-    .send({ 'test': 'testText' })
+    .send({ 'text': 'testText' })
     .end( (err, res) => {
       expect(err).to.eql(null);
       expect(res.status).to.eql(200);
@@ -32,13 +44,16 @@ describe('the server', () => {
       done();
     });
   });
+});
+
+describe('the server errors', () => {
   it('should error 404 on bad requests', (done) => {
     request('localhost:3000')
     .get('/badRoute')
     .end( (err, res) => {
       expect(err).to.not.eql(null);
       expect(res.status).to.eql(404);
-      expect(res.text).to.eql('404 not found');
+      expect(res.text).to.eql('404 error, not found');
       done();
     });
   });
