5ff7760db2b3b62edd212171fdac2846c3bbf9ad
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-04-20 10:42:57 -0700

    adding files(get/post functionality)

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
-// const server = require(__dirname + '/lib/server');
-
-// server(3000);
 
 module.exports = exports = require(__dirname + '/lib/server');
-// module.exports = exports = require(__dirname + '/lib/router');
+
+// console.log('index running');
